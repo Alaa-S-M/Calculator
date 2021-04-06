ab637afb6e9f93239a7294f2bc5c449d886231f7
Alaa-S-M <alaasaabni@gmail.com>
2021-04-06 10:04:02 +0300

    build the basic structure of the calculator

--- calculator/src/components/Calculator.tsx
+++ calculator/src/components/Calculator.tsx
@@ -1,9 +1,34 @@
+import './Calculator.css';
+import Button from './Button';
+import ResultScreen from './ResultScreen';
 
+function Calculator() {
 
-function Calculator(){
     return (
-        <h1>Calculator</h1>
+        <div className='container'>
+            <ResultScreen />
+            <div className='grid'>
+                {buttons.map(row => {
+                    return row.map(cell => {
+                        return (
+                            <div key={cell}>
+                                <Button label={cell} />
+                            </div>
+                        );
+                    })
+                })}
+            </div>
+
+        </div>
     );
 }
 
 export default Calculator;
+
+const buttons = [
+    ['C', '+/-', '%', '÷'],
+    ['7', '8', '9', 'x'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['0', '.', '=']
+];
---
+++ calculator/src/components/ResultScreen.tsx
@@ -0,0 +1,12 @@
+import './ResultScreen.css';
+
+function ResultScreen() {
+    return (
+        <div className='calculator-screen'>
+            <div className='inner-screen'>
+                
+            </div>
+        </div>
+    );
+}
+export default ResultScreen;
